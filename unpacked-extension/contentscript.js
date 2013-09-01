c46ac012d989f70e866e8b9373ef45554407b2f1
Adam Schwartz <aschwartz@hubspot.com>
2013-08-31 20:56:58 -0400

    move styles to external sheet

--- unpacked-extension/contentscript.js
+++ unpacked-extension/contentscript.js
@@ -1,13 +1,18 @@
-var scripts = ['script'], i;
-
 // Only run on Google Image search
 if (/tbm\=isch/.test(location.href)) {
-    scripts.forEach(function(name){
-        var s = document.createElement('script');
-        s.src = chrome.extension.getURL(name + '.js');
-        (document.head||document.documentElement).appendChild(s);
-        s.onload = function() {
-            s.parentNode.removeChild(s);
-        };
-    });
+    // Script
+    var s = document.createElement('script');
+    s.src = chrome.extension.getURL('script.js');
+    (document.head || document.documentElement).appendChild(s);
+    s.onload = function() {
+        s.parentNode.removeChild(s);
+    };
+
+    // Styles
+    var l = document.createElement('link');
+    l.rel = 'stylesheet';
+    l.type = 'text/css';
+    l.className = 'chrome-image-quilts';
+    l.href = chrome.extension.getURL('style.css');
+    (document.head || document.documentElement).appendChild(l);
 }
