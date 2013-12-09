7a56d966d93e50748f17a90a07215bbf15d2bcdc
Zack Bloom <zackbloom@gmail.com>
2013-12-08 22:47:53 -0500

    Add any-page functionality

--- unpacked-extension/js/findImages.js
+++ unpacked-extension/js/findImages.js
@@ -1,2 +1,19 @@
-console.log('y');
-alert('sdf');
+function load(){
+  var images = document.querySelectorAll('img');
+
+  var urls = [];
+  for(var i=images.length; i--;){
+    var image = images[i];
+    urls.unshift(image.src);
+  }
+
+  chrome.runtime.sendMessage({urls: urls}, function(response){
+    console.log('resp');
+  });
+}
+
+if (document.readyState != 'complete'){
+  document.addEventListener('DOMContentLoaded', load);
+} else {
+  load();
+}
--- unpacked-extension/js/popup.js
+++ unpacked-extension/js/popup.js
@@ -1,7 +1,22 @@
+var tray;
+console.log(ffff, '43');
 document.addEventListener('DOMContentLoaded', function(){
+  tray = document.querySelector('.tray');
+
   chrome.windows.getCurrent(function (currentWindow) {
     chrome.tabs.query({active: true, windowId: currentWindow.id}, function(activeTabs) {
-      chrome.tabs.executeScript(activeTabs[0].id, {file: 'findImages.js', allFrames: true});
+      chrome.tabs.executeScript(activeTabs[0].id, {file: 'js/findImages.js', allFrames: true});
     });
   });
+});
+
+chrome.extension.onRequest.addListener(function(images){
+  if (!tray)
+    return;
+
+  for (var i=0; i < images.length; i++){
+    var img = document.createElement('img');
+    img.src = images[i];
+    tray.appendChild(img); 
+  }
 });
